c72889a6a0338eb00d2428ca6895094cb281658a
Andriy-Kozlovsky <reactiivado.-@hotmail.com>
2022-06-21 21:24:08 -0300

    instalando express, rutas, params y querys

--- 4-first-module.js
+++
@@ -1,8 +0,0 @@
-// local
-const secret = "SUPER SECRET";
-// share
-const john = "john";
-const peter = "peter";
-
-// setting the exports prop from the module obj
-module.exports = { john, peter };
